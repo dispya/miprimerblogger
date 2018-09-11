414b389b75c557735bfd74b37234ddc04d0cf04d
dayra saltori <dayra.saltori@uda.cl>
2018-09-11 16:09:30 -0300

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,13 +5,15 @@ def index
   @articles = Article.all
 end
 def show
-@article = Article.find(params[:id])
-
+  @article = Article.find(params[:id])
+  @comment = Comment.new
+  @comment.article_id = @article.id
 end
 def new
 @article = Article.new
 
 end
+
 def create
     @article = Article.new(article_params)
     @article.save
@@ -41,5 +43,5 @@ def update
   
   redirect_to article_path(@article)
 end  
-  
+
 end
